7d750f786a8550db356d611f6c1367e1ced51485
june020503 <june2002kim@naver.com>
2024-12-09 12:28:30 +0900

    Create CountingSort.cpp

---
+++ CountingSort.cpp
@@ -0,0 +1,32 @@
+/*
+https://www.acmicpc.net/problem/15688
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int freq[2000001];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        freq[a+1000000]++;
+    }
+
+    for(int i = 0; i <= 2000000; i++)
+    {
+        while(freq[i]--)
+        {
+            cout << i-1000000 << '\n';
+        }
+    }
+}
